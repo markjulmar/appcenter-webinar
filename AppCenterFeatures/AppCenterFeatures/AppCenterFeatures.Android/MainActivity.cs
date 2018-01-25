13c75cc68c5615f09085e19588ddcc434f250f3b
Mark Smith <mark@julmar.com>
2018-01-25 12:32:03 -0600

    Initial revision

---
+++ AppCenterFeatures/AppCenterFeatures/AppCenterFeatures.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
+using Microsoft.AppCenter.Distribute;
+
+namespace AppCenterFeatures.Droid
+{
+    [Activity(Label = "AppCenterFeatures", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            Push.SetSenderId("31642920974");
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start("2e3ad541-1f14-44ff-a71d-943286857cfd",
+                   typeof(Analytics), typeof(Crashes), typeof(Distribute),
+                   typeof(Push));
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ AppCenterFeatures/AppCenterFeatures/AppCenterFeatures/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AppCenter.Analytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterFeatures
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        static int counter = 0;
+
+        void OnReportProgress(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent("ReportProgress",
+                new Dictionary<string, string>() { { "counter", (counter++).ToString() } });
+        }
+
+        void OnCrash(object sender, EventArgs e)
+        {
+            int n = 0;
+            var results = 1 / n;
+        }
+
+        void OnHandledException(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = null;
+                msg.IndexOf(".");
+            }
+            catch (Exception ex)
+            {
+                ex.Report();
+            }
+        }
+    }
+
+    static class ExceptionHelpers
+    {
+        public static void Report(this Exception ex, [CallerMemberName] string caller = "")
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            Analytics.TrackEvent($"{ex.GetType().Name} ({caller})",
+                    new Dictionary<string, string>()
+                    {
+                        { "message", ex.Message }
+                    });
+        }
+    }
+}
